c1f33e918c5deb8a0f6fe1c16dab685a9cf8da56
Wasif Ansari <wasifansari2709@gmail.com>
2023-01-04 23:21:09 +0530

    month label

--- Digital_Clock.py
+++ Digital_Clock.py
@@ -49,4 +49,14 @@ datelab_txt = Label(clock, text="DATE", font=("Times New Roman",20,"bold"),
 bg='white', fg='black')
 datelab_txt.place(x=120,y=410,height=30,width=100)
 
+monthlab = Label(clock, text="00", font=("Times New Roman",40,"bold"),
+bg='white', fg='black')
+monthlab.place(x=340,y=270,height=110,width=100)
+
+monthlab_txt = Label(clock, text="MONTH", font=("Times New Roman",20,"bold"),
+bg='white', fg='black')
+monthlab_txt.place(x=340,y=410,height=30,width=110)
+
+
+
 clock.mainloop()
