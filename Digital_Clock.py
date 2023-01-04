73093358a43d57b216b17a6149e70ce430c38b73
Wasif Ansari <wasifansari2709@gmail.com>
2023-01-04 23:20:28 +0530

    date label

--- Digital_Clock.py
+++ Digital_Clock.py
@@ -39,5 +39,14 @@ ampm_txt = Label(clock, text="AM/PM", font=("Times New Roman",20,"bold"),
 bg='white', fg='black')
 ampm_txt.place(x=780,y=190,height=30,width=100)
 
+# for date
+
+datelab = Label(clock, text="00", font=("Times New Roman",40,"bold"),
+bg='white', fg='black')
+datelab.place(x=120,y=270,height=110,width=100)
+
+datelab_txt = Label(clock, text="DATE", font=("Times New Roman",20,"bold"),
+bg='white', fg='black')
+datelab_txt.place(x=120,y=410,height=30,width=100)
 
 clock.mainloop()
